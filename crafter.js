a76c87a25d11331e6bc1fe8a8e8c675eefbde8d8
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-12-29 13:47:39 -0500

    Update crafter.js

--- crafter.js
+++ crafter.js
@@ -1,3 +1,104 @@
+function getPromptFromValues() {
+
+    let prompt = "";
+
+    const aiyabot = document.getElementById("aiyabot").checked;
+    const comments = !aiyabot && document.getElementById("comments").checked;
+    const quality = document.getElementById("quality").checked;
+    const isMale = document.getElementById("gender").checked;
+    const view = document.querySelector('input[name="view"]:checked').value;
+    const shot = document.querySelector('input[name="shot"]:checked').value;
+
+    if (aiyabot) {
+        prompt += "/draw prompt:";
+        document.getElementById("comments-section").style.display = "none";
+    } else {
+        document.getElementById("comments-section").style.display = "block";
+    }
+
+    if (comments)
+        prompt += "/* Quality */\n";
+
+    if (quality) {
+        prompt += "best quality, high rating, (high rating), absurd res, high res, detailed, masterpiece, detailed face, intricate detail, sophisticated detail, exquisite detail, absurd resolution, correct anatomy, 8k, (highly detailed face), highly detailed skin texture, gorgeous, perfect hands, ";
+    } else {
+        prompt += "masterpiece, best quality, amazing quality, perfect hands, absurdres, 8k, ";
+    }
+    
+    if (comments)
+        prompt += "\n\n/* Composition */\n";
+
+    if (isMale) {
+        prompt += document.getElementById("char-count").checked ? "2boys, " : "1boy, ";
+    } else {
+        prompt += document.getElementById("char-count").checked ? "2girls, " : "1girl, ";
+    }
+    
+    prompt += "(" + view + ":1.2), ";
+    prompt += shot + ", ";
+    if (document.getElementById("dutch-angle").checked)
+        prompt += "dutch angle, ";
+
+    if (comments)
+        prompt += "\n\n/* Your stuff */\n";
+
+    prompt += document.getElementById("description").value + ", ";
+    
+    if (comments)
+        prompt += "\n\n/* Figure */\n";
+
+    const showBreasts = shot !== "head shot" && !isMale;
+    const showBelly = shot !== "head shot";
+    const showHips = shot === "full body shot" || shot === "medium shot";
+    const includeAss = view === "side view" || view === "view from behind";
+    const showLegs = shot === "full body shot";
+    const showAnything = showBreasts || showBelly || showHips || showLegs;
+    
+    if (showAnything)
+        prompt += "(";
+    
+    if (showBreasts) {
+        prompt += constructBreastsPrompt(document.getElementById("breasts-thickness").value);
+        document.getElementById("breasts-section").style.display = "table-row";
+    } else {
+        document.getElementById("breasts-section").style.display = "none";
+    }
+
+    if (showBelly) {
+
+        if (showBreasts)
+            prompt += ", ";
+        
+        prompt += constructBellyPrompt(document.getElementById("belly-thickness").value);
+        document.getElementById("belly-section").style.display = "table-row";
+    } else {
+        document.getElementById("belly-section").style.display = "none";
+    }
+    
+    if (showHips) {
+        prompt += ", " + constructHipsPrompt(document.getElementById("hips-thickness").value, includeAss);
+        document.getElementById("hips-section").style.display = "table-row";
+    } else {
+        document.getElementById("hips-section").style.display = "none";
+    }
+
+    if (showLegs) {
+        prompt += ", " + constructLegsPrompt(document.getElementById("legs-thickness").value);
+        document.getElementById("legs-section").style.display = "table-row";
+    } else {
+        document.getElementById("legs-section").style.display = "none";
+    }
+
+    if (showAnything)
+        prompt += ":1.3)";
+
+    if (aiyabot)
+        prompt += ` width:${ document.getElementById("width").value } height:${ document.getElementById("height").value }`;
+
+    return prompt;
+}
+
+
 // const artists = [
 //     { display_name: "blushyspicy",    value: "blushyspicy(artist)", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgK8Lx11D2D8azuT1eM_l4GytBvz3OaDgcZg&s" },
 //     { display_name: "c.cu",           value: "c.cu(artist)", image: "https://cdn.donmai.us/original/17/b1/17b1e5ee5aca849222dc07769ce8fc1c.jpg" },
@@ -65,103 +166,4 @@ function constructLegsPrompt(value) {
     }
     
     return extra + getSize(value) + " calves";
-}
-
-function getPromptFromValues() {
-
-    const output = document.getElementById("output");
-    output.value = "";
-
-    const aiyabot = document.getElementById("aiyabot").checked;
-    const comments = !aiyabot && document.getElementById("comments").checked;
-    const quality = document.getElementById("quality").checked;
-    const isMale = document.getElementById("gender").checked;
-    const view = document.querySelector('input[name="view"]:checked').value;
-    const shot = document.querySelector('input[name="shot"]:checked').value;
-
-    if (aiyabot) {
-        output.value += "/draw prompt:";
-        document.getElementById("comments-section").style.display = "none";
-    } else {
-        document.getElementById("comments-section").style.display = "block";
-    }
-
-    if (comments)
-        output.value += "/* Quality */\n";
-
-    if (quality) {
-        output.value += "best quality, high rating, (high rating), absurd res, high res, detailed, masterpiece, detailed face, intricate detail, sophisticated detail, exquisite detail, absurd resolution, correct anatomy, 8k, (highly detailed face), highly detailed skin texture, gorgeous, perfect hands, ";
-    } else {
-        output.value += "masterpiece, best quality, amazing quality, perfect hands, absurdres, 8k, ";
-    }
-    
-    if (comments)
-        output.value += "\n\n/* Composition */\n";
-
-    if (isMale) {
-        output.value += document.getElementById("char-count").checked ? "2boys, " : "1boy, ";
-    } else {
-        output.value += document.getElementById("char-count").checked ? "2girls, " : "1girl, ";
-    }
-    
-    output.value += "(" + view + ":1.2), ";
-    output.value += shot + ", ";
-    if (document.getElementById("dutch-angle").checked)
-        output.value += "dutch angle, ";
-
-    if (comments)
-        output.value += "\n\n/* Your stuff */\n";
-
-    output.value += document.getElementById("description").value + ", ";
-    
-    if (comments)
-        output.value += "\n\n/* Figure */\n";
-
-    const showBreasts = shot !== "head shot" && !isMale;
-    const showBelly = shot !== "head shot";
-    const showHips = shot === "full body shot" || shot === "medium shot";
-    const includeAss = view === "side view" || view === "view from behind";
-    const showLegs = shot === "full body shot";
-    const showAnything = showBreasts || showBelly || showHips || showLegs;
-    
-    if (showAnything)
-        output.value += "(";
-    
-    if (showBreasts) {
-        output.value += constructBreastsPrompt(document.getElementById("breasts-thickness").value);
-        document.getElementById("breasts-section").style.display = "table-row";
-    } else {
-        document.getElementById("breasts-section").style.display = "none";
-    }
-
-    if (showBelly) {
-
-        if (showBreasts)
-            output.value += ", ";
-        
-        output.value += constructBellyPrompt(document.getElementById("belly-thickness").value);
-        document.getElementById("belly-section").style.display = "table-row";
-    } else {
-        document.getElementById("belly-section").style.display = "none";
-    }
-    
-    if (showHips) {
-        output.value += ", " + constructHipsPrompt(document.getElementById("hips-thickness").value, includeAss);
-        document.getElementById("hips-section").style.display = "table-row";
-    } else {
-        document.getElementById("hips-section").style.display = "none";
-    }
-
-    if (showLegs) {
-        output.value += ", " + constructLegsPrompt(document.getElementById("legs-thickness").value);
-        document.getElementById("legs-section").style.display = "table-row";
-    } else {
-        document.getElementById("legs-section").style.display = "none";
-    }
-
-    if (showAnything)
-        output.value += ":1.3)";
-
-    if (aiyabot)
-        output.value += ` width:${ document.getElementById("width").value } height:${ document.getElementById("height").value }`;
 }
