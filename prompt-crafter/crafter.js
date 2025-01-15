6279af448a06ec24c73f85d30d0422b8eddcb350
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2025-01-15 00:32:40 -0500

    Update crafter.js

--- prompt-crafter/crafter.js
+++ prompt-crafter/crafter.js
@@ -1,16 +1,4 @@
-// if (requestComments)
-//     promptInfo.prompt += "/* Quality */\n";
-// promptInfo.prompt += constructQualityPrompt(requestHighQuality) + ", ";
-
-// if (artistsPrompt !== "") {
-
-//     if (requestComments)
-//         promptInfo.prompt += "\n\n/* Artists */\n";
-    
-//     promptInfo.prompt += artistsPrompt + ", ";
-// }
-
-function getQualityInfo() {
+function getQualityPresets() {
 
     return [
         { name: "Simple",  prompt: "masterpiece, best quality, amazing quality, perfect hands, absurdres, 8k" },
@@ -18,7 +6,23 @@ function getQualityInfo() {
     ];
 }
 
-function getArtistsInfo() {
+function getFigurePresets() {
+
+    // figure setting presets like "Vore," "Pregnant," "Hourglass," and of course "Thicc Girl"
+    
+    // fat, obese, big belly, deep navel, large round breasts
+    // fat neck, fat face, (vore belly:1.5)
+    // chubby
+    // (large belly, round belly, stuffed belly, pot belly, belly sticking out, fat)
+    // thick thighs
+    // "small", "slightly big", "big", "slightly huge", "huge", "slightly massive", "massive", "hyper" ]; // giant?
+
+    return [
+        { name: "Hourglass",  prompt: "(slim, no belly, narrow waist)" }
+    ];
+}
+
+function getArtistsPresets() {
 
     // c.cu, kakuteki, kipteitei, cookiescat, blushyspicy, nekocrispy, stunnerpony, inu-sama, the dogsmith, tsukasawa takamatsu
     // c.cu (artist), inkerton-kun(artist), mdf_an(artist), wamudraws(artist), dogsmith(artist), inu-sama(artist), faizenek(artist)
@@ -41,129 +45,69 @@ function getArtistsInfo() {
     ];
 }
 
-function getPromptInfo(initPacket, promptPackets) {
 
-    let promptInfo = {
-        prompt: "",
-        consideredBreasts: true, //shotPrompt !== "head shot" && !isMale,
-        consideredBelly:   true, //shotPrompt !== "head shot",
-        consideredHips:    true  //shotPrompt === "full body shot" || shotPrompt === "medium shot"
-    };
+
+function getPrompt(initPacket, promptPackets) {
+
+    let prompt = "";
 
     const comments = initPacket.format === "commented";
 
     if (initPacket.format === "aiyabot")
-        promptInfo.prompt += "/draw prompt:";
+        prompt += "/draw prompt:";
     
     for (const packet of promptPackets) {
 
         if (comments)
-            promptInfo.prompt += "/* " + packet.packetName.substring(0, 1).toUpperCase() + packet.packetName.substring(1) + " */\n";
+            prompt += "/* " + packet.packetName.substring(0, 1).toUpperCase() + packet.packetName.substring(1) + " */\n";
 
         switch (packet.packetName) {
 
-            case "description": promptInfo.prompt += packet.description + ", "; break;
+            case "description": prompt += packet.description + ", "; break;
 
-            case "quality":     promptInfo.prompt += getQualityInfo()[packet.index].prompt + ", "; break;
+            case "quality":     prompt += getQualityPresets()[packet.index].prompt + ", "; break;
                 
-            case "figure":      appendFigurePrompt(promptInfo, packet); break;
+            case "figure":      prompt += getFigurePresets()[packet.index].prompt + ", " break;
+            
+            case "artists":     prompt += getArtistsPresets()[packet.index].prompt + ", "; break;
                 
-            case "artists":     promptInfo.prompt += getArtistsInfo()[packet.index].prompt + ", "; break;
-                
-            case "composition": appendCompositionPrompt(promptInfo, packet); break;
+            case "composition": prompt += getCompositionPrompt(packet) + ", "; break;
                 
             default:            console.log("Recieved unexpected packet: " + packet.packetName);
         }
 
         if (comments)
-            promptInfo.prompt += "\n\n";
+            prompt += "\n\n";
     }
 
-    if (promptInfo.prompt.length >= 2) {
-
-        promptInfo.prompt = promptInfo.prompt.substring(0, promptInfo.prompt.length - (comments ? 4 : 2));
-    }
+    if (prompt.length >= 2)
+        prompt = prompt.substring(0, prompt.length - (comments ? 4 : 2));
 
     if (initPacket.format === "aiyabot") {
 
         if (initPacket.useNegatives)
-            promptInfo.prompt += " negative_prompt:bad anatomy, bad hands, text, missing finger, extra digits, fewer digits, mutated hands, mutated fingers, poorly drawn face, mutation, deformed face, ugly, bad proportions, bad anatomy, bad hands, text, error, missing fingers, cropped, censored, pixelart, ugly, sketch, lineart, monochrome, duplicate, morbid, mutilated, mutated, missing limbs, missing limb, monster, logo, print, cropped, (worst quality, low quality:1.4), extra fingers, fewer fingers, (bad eyes:1.2), (misfigured pupils:1.2), (bad clothing:1.3), (nonsensical backgrounds:1.2), (bad backgrounds:1.2), (bad shadows:1.2), (bad anatomy:1.1), jpeg artifacts, signature, watermark, username, blurry, out of frame, screenshot, game cg, user interface, source request, commentary request, english commentary, symbol-only commentary, commentary, commission, bad composition, inaccurate eyes, (extra arms:1.2)";
+            prompt += " negative_prompt:bad anatomy, bad hands, text, missing finger, extra digits, fewer digits, mutated hands, mutated fingers, poorly drawn face, mutation, deformed face, ugly, bad proportions, bad anatomy, bad hands, text, error, missing fingers, cropped, censored, pixelart, ugly, sketch, lineart, monochrome, duplicate, morbid, mutilated, mutated, missing limbs, missing limb, monster, logo, print, cropped, (worst quality, low quality:1.4), extra fingers, fewer fingers, (bad eyes:1.2), (misfigured pupils:1.2), (bad clothing:1.3), (nonsensical backgrounds:1.2), (bad backgrounds:1.2), (bad shadows:1.2), (bad anatomy:1.1), jpeg artifacts, signature, watermark, username, blurry, out of frame, screenshot, game cg, user interface, source request, commentary request, english commentary, symbol-only commentary, commentary, commission, bad composition, inaccurate eyes, (extra arms:1.2)";
             // previous one: " negative_prompt:(ugly), ((watermark, hourglass)), ((mutilated)), out of frame, extra fingers, extra limbs, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), ((mutation)), ((deformed)), blurry, (bad anatomy), (bad proportions), (extra limbs), (disfigured), (malformed limbs), ((missing arms)), ((missing legs)), ((extra arms)), ((extra legs)), (fused fingers), (too many fingers), (long neck), bad_eyes, poorly_drawn_eyes";
       
-        promptInfo.prompt += ` width:${ initPacket.width } height:${ initPacket.height }`;
+        prompt += ` width:${ initPacket.width } height:${ initPacket.height }`;
     }
     
-    return promptInfo;
+    return prompt;
 }
 
-function appendCompositionPrompt(promptInfo, packet) {
+function getCompositionPrompt(packet) {
 
     // looking at viewer, (1girl, solo), (front view, medium shot, dutch angle:1.5),
 
+    let construct = "";
+    
     if (packet.isMale) {
-        promptInfo.prompt += packet.isPair ? "(2boys)" : "(1boy, solo)";
+        construct += packet.isPair ? "(2boys)" : "(1boy, solo)";
     } else {
-        promptInfo.prompt += packet.isPair ? "(2girls)" : "(1girl, solo)";
+        construct += packet.isPair ? "(2girls)" : "(1girl, solo)";
     }
     
-    promptInfo.prompt += ", (" + packet.viewPrompt + ", " + packet.shotPrompt + (packet.isDutchAngle ? ", dutch angle:1.5), " : ":1.5), ");
-}
+    construct += ", (" + packet.viewPrompt + ", " + packet.shotPrompt + (packet.isDutchAngle ? ", dutch angle:1.5)" : ":1.5)");
 
-function appendFigurePrompt(promptInfo, packet) {
-    
-    if (promptInfo.consideredBreasts)
-        promptInfo.prompt += constructBreastsPrompt(packet.breasts) + ", ";
-
-    if (promptInfo.consideredBelly)
-        promptInfo.prompt += constructBellyPrompt(packet.belly) + ", ";
-    
-    if (promptInfo.consideredHips)
-        promptInfo.prompt += constructHipsPrompt(packet.hips) + ", ";
-}
-
-// TODO redo the size system since it doesn't really work well for Aurora Borealis
-// fat, obese, big belly, deep navel, large round breasts
-
-const sizeHeirarchy = [ "small", "slightly big", "big", "slightly huge", "huge", "slightly massive", "massive", "hyper" ]; // giant?
-
-function getSize(value) {
-    return sizeHeirarchy[parseInt((value * sizeHeirarchy.length - 1) / 100.0)];
-}
-
-// value is in range [1, 100]
-function constructBreastsPrompt(value) {
-
-    if (value == 1)
-        return "(flat chest)"; // "no breasts"?
-
-    return "(" + getSize(value) + " breasts:1.3)";
-}
-
-function constructBellyPrompt(value) {
-    
-    if (value == 1)
-        return "(slim, no belly, hourglass figure, narrow waist)";
-
-    if (value < 8)
-        return "(slim)";
-    
-    if (value < 16)
-        return "(chubby)";
-
-    let extra = "";
-
-    if (value > 80)
-        extra += "(large belly, round belly, stuffed belly, pot belly, belly sticking out, fat), ";
-    
-    return extra + "(" + getSize(value) + " belly:1.3)";
-}
-
-function constructHipsPrompt(value) {
-    
-    let extra = "";
-
-    if (value >= 60)
-        extra += "(thick thighs:1." + parseInt(value / 10 - 5) + "), ";
-    
-    return extra + "(" + getSize(value) + " hips, " + getSize(value) + " ass" + ":1.3)";
+    return construct;
 }
